88c7595da9dd3b096ab973ea3b935d8e00f3052c
Christian <cpaskevicius@vbg.com>
2026-05-07 10:08:40 -0700

    Added items to git ignore

--- dbt-cloud-managed-repositories-dbt-cloud-70471823547411-jaffle-shop-8fe54170dc8093f22bfd41cd72d8ed4cc1a500e5/macros/generate_schema_name.sql
+++ dbt-cloud-managed-repositories-dbt-cloud-70471823547411-jaffle-shop-8fe54170dc8093f22bfd41cd72d8ed4cc1a500e5/macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- set env=env_var('DBT_ENV_NAME') -%}
+    {%- set env=env_var('DBT_ENV_NAME', 'dev') -%}
 
     {%- if custom_schema_name is none or env == 'dev' -%}
 
